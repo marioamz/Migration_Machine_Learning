7fd91f686ef1afdec0d3786de71e59f220deb652
Mario Moreno <marioamz@gmail.com>
2020-02-11 15:09:19 -0500

    changes

--- LAPOP/Total_Services.R
+++ LAPOP/Total_Services.R
@@ -2,6 +2,7 @@ library(here)
 library(ggplot2)
 library(reshape2)
 library(gghighlight)
+library(gganimate)
 
 # Read in intent to migrate data
 services <- read.csv(here("LAPOP/Data/services_total.csv"), check.names = FALSE)
@@ -9,6 +10,8 @@ services <- read.csv(here("LAPOP/Data/services_total.csv"), check.names = FALSE)
 wola_blue <- '#00b5ef'
 wola_grey <- '#d5d0ca'
 wola_text <- "#404042"
+wola_purple <- '#603F99'
+wola_green <- '#65BD60'
 
 wola_theme <- theme(text = element_text(family='Arial'),
                     plot.background = element_blank(),
@@ -29,8 +32,8 @@ wola_theme <- theme(text = element_text(family='Arial'),
 plot <- ggplot(services, aes(x=wave, y=perc_mig, group = pais, label = pais)) + 
   geom_line(aes(color = pais), size = 1) + scale_color_manual(values = c(wola_grey, wola_purple, wola_grey)) + 
   geom_text() + transition_reveal(wave) +
-  labs(title = 'Guatemalans increasingly think local government\n services are poor',
-       subtitle = 'Hondurans and Salvadorians are similarly frustrated with local\n services, but not as drastically',
+  labs(title = 'Guatemalans increasingly think local \n government services are poor',
+       subtitle = 'Hondurans and Salvadorians are similarly frustrated \n with local services, but not as drastically',
        caption = "Source: SGL1 in LAPOP surveys, \n asking people's assessment of municipal services") + 
   xlab('Year') + ylab('Percent responding "Bad" or "Very Bad"') 
 
